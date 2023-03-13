6b8648ed9073dcbb1f05b8a5f4dcdbfb099ea1a5
尸府主任 <gfiszhang@gmail.com>
2023-03-13 17:52:11 +0800

    2023-03-13

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
-use std::env;
+use std::{env};
+
+static mut PARAMETERS: Vec<String> = vec![];
+static mut OPTIONS: Vec<String> = vec![];
+static mut FLAGS: Vec<String> = vec![];
+
+// let a: i32 = 100;
 
 fn main() {
-    star(env::args().collect())
+    let args = env::args().collect();
+    star(&args);
 }
 
+fn star(args: &Vec<String>) {
+    let len=args.len();
+    for i in 1..len {
+        let current = &args[i];
 
-fn star(args: Vec<String>) {
-    println!("{:?}", args);
+        if !current.starts_with("-") {
+            unsafe {
+                PARAMETERS.push(current.clone());
+            }
+            continue;
+        }
+
+        if current.contains("=") {
+            unsafe {
+                OPTIONS.push(current.clone());
+            }
+            continue;
+        }
+
+        unsafe {
+            FLAGS.push(current.clone());
+        }
+
+    }
+    
+    unsafe {
+        println!("P: {:?}", PARAMETERS);
+        println!("O: {:?}", OPTIONS);
+        println!("F: {:?}", FLAGS);
+
+    }
 }
